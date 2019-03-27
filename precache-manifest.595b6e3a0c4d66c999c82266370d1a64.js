8bb46f234e1b2c9b139c3d5f768aa51b88d68ead
Kamil Kawałkiewicz <kamil.kawalkiewicz@gmial.com>
2019-03-27 20:40:28 +0100

    Updates

---
+++ precache-manifest.595b6e3a0c4d66c999c82266370d1a64.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5bafd9663c0fe72a8a847a54c1990c6",
+    "url": "/physiotherapySide/./static/media/sunblock_640.b5bafd96.jpg"
+  },
+  {
+    "revision": "136f30627170e535e6ed",
+    "url": "/physiotherapySide/./static/css/main.fc65891b.chunk.css"
+  },
+  {
+    "revision": "037d501b686bfd162447",
+    "url": "/physiotherapySide/./static/js/runtime~main.037d501b.js"
+  },
+  {
+    "revision": "de9a86dc179424b408c4",
+    "url": "/physiotherapySide/./static/js/2.de9a86dc.chunk.js"
+  },
+  {
+    "revision": "ec72186d3e36499bed37d3aa30ec02cd",
+    "url": "/physiotherapySide/./static/media/opinion3.ec72186d.png"
+  },
+  {
+    "revision": "dd18eba4abbfa1c3bea22261221c1cec",
+    "url": "/physiotherapySide/./static/media/opinion6.dd18eba4.jpg"
+  },
+  {
+    "revision": "eb0aac754be160f73ac2cc11dc241234",
+    "url": "/physiotherapySide/./static/media/header1.eb0aac75.png"
+  },
+  {
+    "revision": "24992b3db3750499b412e9089678f5dd",
+    "url": "/physiotherapySide/./static/media/pain_640.24992b3d.jpg"
+  },
+  {
+    "revision": "136f30627170e535e6ed",
+    "url": "/physiotherapySide/./static/js/main.136f3062.chunk.js"
+  },
+  {
+    "revision": "035b599effa1c97d83603854543caf3a",
+    "url": "/physiotherapySide/./static/media/TJ.035b599e.jpg"
+  },
+  {
+    "revision": "696f04e6672e9cfd577f9706bca60682",
+    "url": "/physiotherapySide/./static/media/Articles1.696f04e6.jpg"
+  },
+  {
+    "revision": "5ad469ee3f5f110c7ed1946f8c81a7fd",
+    "url": "/physiotherapySide/./static/media/Articles2.5ad469ee.jpg"
+  },
+  {
+    "revision": "728b1d4a86a677599ea673e7f0e37871",
+    "url": "/physiotherapySide/./static/media/Articles3.728b1d4a.jpg"
+  },
+  {
+    "revision": "ecc4ceedb526357cedd30c2e4b03e7ec",
+    "url": "/physiotherapySide/./static/media/Articles4.ecc4ceed.jpg"
+  },
+  {
+    "revision": "762db38d1f810f84e8b55ae1ba31650f",
+    "url": "/physiotherapySide/./static/media/Articles5.762db38d.jpg"
+  },
+  {
+    "revision": "1054e6c5899a34c8eacde67b2f170a4a",
+    "url": "/physiotherapySide/./static/media/AnatomyImg.1054e6c5.png"
+  },
+  {
+    "revision": "cba968e28838b3edb091bacd96fb7fe1",
+    "url": "/physiotherapySide/./index.html"
+  }
+];
